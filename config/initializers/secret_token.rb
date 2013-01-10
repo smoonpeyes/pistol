9b58cf70da28b2abdeec12ac770a27803d614b70
Layla Soileau <smoonpeyes@gmail.com>
2013-01-10 06:10:53 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pistol::Application.config.secret_token = '0987f0ac968eb9deb39ae1399da8b2b9a1d1a3db5d2179065f5c34d470eeede4dd6c4553a6676b68dfc0176ff2dca057af922655ff355c527781935cfa7cd50a'
